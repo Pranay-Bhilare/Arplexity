1186da6426804c6a810d469506e909d167ca4410
bhilare_ <bhilarespranay81@gmail.com>
2025-06-29 10:49:53 +0530

    Implemented graph of agent

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from typing import TypedDict, Annotated, Optional
-from langgraph.graph import add_messages, StateGraph, END
+from langgraph.graph import add_messages, StateGraph, START
+from langgraph.prebuilt import ToolNode, tools_condition
 from langchain_groq import ChatGroq
 from langchain_core.messages import HumanMessage, AIMessageChunk, ToolMessage
 from dotenv import load_dotenv
@@ -11,3 +12,33 @@ import json
 from uuid import uuid4
 from langgraph.checkpoint.memory import MemorySaver
 
+load_dotenv()
+
+memory = MemorySaver()
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+search_tool = TavilySearchResults(
+    max_results=4,
+)
+tools = [search_tool]
+llm = ChatGroq(model="")
+
+llm_with_tools = llm.bind_tools(tools=tools)
+
+
+async def model(state: State) : 
+    response = await llm_with_tools.ainvoke(state["messages"])  
+    return {
+        "messages": [response], 
+    }
+
+graph_builder = StateGraph(State)
+graph_builder.add_node("llm", model)
+graph_builder.add_node("tools", ToolNode(tools))
+graph_builder.add_edge(START,"llm")
+graph_builder.add_conditional_edges("llm",tools_condition)
+graph_builder.add_edge("tools", "llm")
+
+graph = graph_builder.compile(checkpointer=memory)
