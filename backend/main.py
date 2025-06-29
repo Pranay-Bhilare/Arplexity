b007c269a9df713a0f57fb778671e0c9b5d51925
bhilare_ <bhilarespranay81@gmail.com>
2025-06-29 10:32:01 +0530

    Added requirements

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from arplexity!")
+from typing import TypedDict, Annotated, Optional
+from langgraph.graph import add_messages, StateGraph, END
+from langchain_groq import ChatGroq
+from langchain_core.messages import HumanMessage, AIMessageChunk, ToolMessage
+from dotenv import load_dotenv
+from langchain_community.tools.tavily_search import TavilySearchResults
+from fastapi import FastAPI, Query
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+import json
+from uuid import uuid4
+from langgraph.checkpoint.memory import MemorySaver
 
-
-if __name__ == "__main__":
-    main()
